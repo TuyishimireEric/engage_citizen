cd71019361b0b7878bfd0800dfa980068570711b
Tuyishimire Eric <tuyishimireericc@gmail.com>
2025-05-19 05:09:33 +0200

    Removed middleware

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,68 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.learnenglish.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.getmidnight.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "blogassets.leverageedu.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.signfix.com.au",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
